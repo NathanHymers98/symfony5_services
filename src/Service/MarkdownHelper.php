dbfe05a36024c858a63b7fab966bbe27c949e6af
Nathan Hymers <nathan.hymers@dgkc2hgcjv3x.wren.wrenkitchens.com>
2020-08-25 15:26:48 +0100

    second commit

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class MarkdownHelper
+{
+    private $markdownParser; // To do dependency injection, make properties, make a constructor with the objects you want to use passed to it then simply use those properties in the class
+    private $cache;
+    private $isDebug;
+    private $markdownLogger;
+
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger) // This is how you would do dependency injection so that we can use objects in a service class
+    {
+        $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        $this->logger = $markdownLogger;
+    }
+
+    public function parse(string $source): string
+    {
+        if (stripos($source, 'cat') !== false) {
+            $this->markdownLogger->info('Meow');
+        }
+
+        if($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+
+        return $this->cache->get('markdown_'.md5($source), function() use($source){
+            return $this->markdownParser->transformMarkdown($source);
+        });
+    }
+}
